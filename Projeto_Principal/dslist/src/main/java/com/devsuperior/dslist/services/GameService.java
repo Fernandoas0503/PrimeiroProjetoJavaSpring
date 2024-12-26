6f9fae812c547e5c8e0c78ad6b3c2f055a604c24
unknown <fernandoas0503@gmail.com>
2024-12-26 14:46:49 -0300

    Game repository, service, controller

--- Projeto_Principal/dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
+++ Projeto_Principal/dslist/src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -3,19 +3,20 @@ package com.devsuperior.dslist.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
+import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
 
-@Component
+@Service
 public class GameService {
 
 	@Autowired
 	private GameRepository gameRepository;
 	
-	public List<Game> findAll() {
+	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
-		return result;
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
