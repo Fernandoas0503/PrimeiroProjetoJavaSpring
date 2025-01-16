2f4a03e67be89563aad351c0016838faf50816d7
unknown <fernandoas0503@gmail.com>
2025-01-16 14:36:11 -0300

    Classes dslist repository e services alteradas

--- Projeto_Principal/dslist/src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ Projeto_Principal/dslist/src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameListRepository;
 
 @Service
@@ -20,5 +21,21 @@ public class GameListService {
 	public List<GameListDTO> findAll() {
 		List<GameList> result = gameListRepository.findAll();
 		return result.stream().map(x -> new GameListDTO(x)).toList();
+	}
+	
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for (int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
+		
 	}
 }
