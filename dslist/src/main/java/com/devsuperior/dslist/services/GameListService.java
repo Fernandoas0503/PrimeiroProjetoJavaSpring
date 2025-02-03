ba7a5079ca21bb303d0066f0ad29012bde179951
unknown <fernandoas0503@gmail.com>
2025-02-03 16:07:35 -0300

    Atualizações nos repositorys

--- dslist/src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ dslist/src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -10,12 +10,16 @@ import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.entities.GameList;
 import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
 
 	@Autowired
 	private GameListRepository gameListRepository;
+	
+	@Autowired
+	private GameRepository gameRepository;
 		
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll() {
